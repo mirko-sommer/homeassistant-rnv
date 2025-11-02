b8af5453a8baf4a432b6ac9ca88ec1f7dcd694e8
Johannes Kast <mail@johannes-kast.de>
2025-11-02 09:14:15 +0100

    check format upon ok press

--- custom_components/rnv/config_flow.py
+++ custom_components/rnv/config_flow.py
@@ -245,19 +245,28 @@ class ConfigFlow(ConfigFlow, domain=DOMAIN):
         errors: dict[str, str] = {}
 
         if user_input is not None:
-            try:
-                info = await validate_input(self.hass, user_input)
-            except CannotConnect:
-                errors["base"] = "cannot_connect"
-            except InvalidAuth:
-                errors["base"] = "invalid_auth"
-            except Exception:  # pylint: disable=broad-except
-                _LOGGER.exception("Unexpected exception")
-                errors["base"] = "unknown"
-            else:
-                return self.async_create_entry(
-                    title=info["title"], data=user_input | {"at_info": info["at_info"]}
-                )
+            sanitized: dict[str, Any] = {}
+            for key, raw_value in user_input.items():
+                try:
+                    sanitized[key] = sanitize_credential(key, raw_value)
+                except InvalidCredentialFormat:
+                    errors[key] = "invalid_format"
+
+            if not errors:
+                try:
+                    info = await validate_input(self.hass, sanitized)
+                except CannotConnect:
+                    errors["base"] = "cannot_connect"
+                except InvalidAuth:
+                    errors["base"] = "invalid_auth"
+                except Exception:  # pylint: disable=broad-except
+                    _LOGGER.exception("Unexpected exception")
+                    errors["base"] = "unknown"
+                else:
+                    return self.async_create_entry(
+                        title=info["title"],
+                        data=sanitized | {"at_info": info["at_info"]},
+                    )
 
         return self.async_show_form(
             step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
